48847316ff69a92e2fa7286177c399eb0281915e
pangjm <pangjm@xiaopeng.com>
2022-07-26 15:39:07 +0800

    全部的项目都进行上传

---
+++ ViewPageDemo/app/src/main/java/com/example/viewpagedemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.viewpagedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.viewpagedemo.activity.ViewPagerTestActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void jumpToViewPage(View view) {
+        startActivity(new Intent(this, ViewPagerTestActivity.class));
+    }
+}
